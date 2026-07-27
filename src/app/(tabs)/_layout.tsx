59767497b5beda0699c4290e7b482cef0c28c375
Miguel Ángel <gomezbenitezmiguelangel@outlook.com>
2026-07-27 13:06:15 +0200

    feat: localize tab navigation labels and update mobile ad configuration keys

--- src/app/(tabs)/_layout.tsx
+++ src/app/(tabs)/_layout.tsx
@@ -1,14 +1,18 @@
 import { Octicons } from "@expo/vector-icons";
 import { Tabs } from "expo-router";
+import { useTranslation } from "react-i18next";
 
 export default function TabLayout() {
+    const { t } = useTranslation();
     return (
         <Tabs screenOptions={{ headerShown: false }}>
             <Tabs.Screen name="index" options={
                 {
+                    title: t("home"),
                     tabBarIcon: ({ color, size }) => (
                         <Octicons name="home" size={size} color={color} />
                     ),
+                    
                 }
             } />
         </Tabs>
