07b2045799025f5cc2741092719896c9f6498f0e
Miguel Ángel <gomezbenitezmiguelangel@outlook.com>
2026-08-18 10:46:13 +0200

    style: center inbox screen content using flexbox utilities

--- src/app/(home)/(SocialMode)/(tabs)/messages/inbox.tsx
+++ src/app/(home)/(SocialMode)/(tabs)/messages/inbox.tsx
@@ -6,7 +6,7 @@ import { Text, View } from "react-native";
 
 export default function Inbox() {
     return (
-        <View>
+        <View className={`flex-1 items-center justify-center`} >
             <Text>Inbox</Text>
         </View>
     )
